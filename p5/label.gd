caec2b5b814395e134ecce8c311893ae0639c139
Luís Eduardo <luiseduardo0501@protonmail.com>
2025-11-29 17:24:14 -0300

    pagina 5

---
+++ p5/label.gd
@@ -0,0 +1,50 @@
+extends Label
+
+var passos = 0
+var pode_contar = true
+var liberado = false
+
+const FORCA_PARA_PASSO = 12.0
+const TEMPO_ENTRE_PASSOS = 0.2
+@onready var sapato = $AnimatedSprite2D
+@onready var comentarios = $comentarios
+@onready var botaoPublicar = $publicar
+
+func _ready():
+	text = "0"
+	sapato.play("parado")
+	$publicar.texture_normal = preload("res://p4/publicarDesligado.png")
+
+func _process(delta):
+	var aceleracao = Input.get_accelerometer()
+	var forca_total = aceleracao.length()
+	var detectou_movimento = (forca_total > FORCA_PARA_PASSO)
+	
+	if Input.is_action_just_pressed("ui_accept"): 
+		detectou_movimento = true
+	
+	if detectou_movimento and pode_contar:
+		contar_passo()
+	
+	if passos > 0:
+		botaoPublicar.texture_normal = preload("res://p4/publicarLIgado.png")
+		liberado = true
+
+func contar_passo():
+	sapato.play("correndo")
+	passos += 1
+	text = str(passos)
+	#print("Passo detectado! Total: ", passos)
+	
+	pode_contar = false
+	
+	await get_tree().create_timer(TEMPO_ENTRE_PASSOS).timeout
+	
+	pode_contar = true
+
+
+func _on_publicar_pressed() -> void:
+	if liberado:
+		comentarios.visible = true
+	else:
+		pass
--- p5/pagina_5.gd
+++ p5/pagina_5.gd
@@ -1,9 +1,9 @@
 extends TextureRect
 
 
-func _on_touch_screen_button_pressed() -> void:
+func _on_voltar_pressed() -> void:
 	get_tree().change_scene_to_file("res://p4/pagina4.tscn")
 
 
-func _on_touch_screen_button_2_pressed() -> void:
+func _on_seguir_pressed() -> void:
 	get_tree().change_scene_to_file("res://p6/pagina6.tscn")
