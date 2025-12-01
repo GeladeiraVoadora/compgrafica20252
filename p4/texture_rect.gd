07a5158032fdf34671e4bef48a055d5bbddfeebd
Luís Eduardo <luiseduardo0501@protonmail.com>
2025-12-01 16:36:05 -0300

    delet: script

--- p4/texture_rect.gd
+++
@@ -1,9 +0,0 @@
-extends TextureRect
-
-
-func _on_potenciometro_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_potenciometro_released() -> void:
-	pass # Replace with function body.
